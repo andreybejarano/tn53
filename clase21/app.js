3251a46dc3d0f817d01c81ab619d711eb69ef1ae
Andrey Bejarano <andreybejarano@gmail.com>
2023-02-17 11:39:07 -0300

    clase21 clase22

---
+++ clase21/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+
+const publicPath = path.join(__dirname, 'public');
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server started on http://localhost:${port}`);
+});
