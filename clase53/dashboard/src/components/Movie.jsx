0d85d2d1297c0ab6baf52d0b77bc10e77fc4bce5
Andrey Bejarano <andreybejarano@gmail.com>
2023-05-26 06:03:39 +0300

    clase53

---
+++ clase53/dashboard/src/components/Movie.jsx
@@ -0,0 +1,60 @@
+//Trabajando con componentes de estado - Clases
+//Apis  - Eventos
+import React from 'react'
+
+//Importar nuestro componente
+import MovieList from './MovieList'
+
+class Movie extends React.Component {
+    state = {
+        movies: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3001/api/movies')
+        .then(res => res.json())
+        .then(data => {
+            this.setState({ movies: data.data })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
+    render() {
+        return (
+            <>
+                {/*<!-- MOVIES LIST -->*/}
+                <h1 className="h3 mb-2 text-gray-800 ">All the movies in the Database</h1>
+
+                {/*<!-- DataTales Example -->*/}
+                <div className="card shadow mb-4">
+                    <div className="card-body">
+                        <div className="table-responsive">
+                            <table className="table table-bordered" id="dataTable" width="100%" cellSpacing="0">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Titulo</th>
+                                        <th>Calificación</th>
+                                        <th>Premios</th>
+                                        <th>Duración</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {
+                                        this.state.movies.map((movie, index) => {
+                                            return <MovieList  {...movie} key={index} />
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </>
+        )
+    }
+}
+
+export default Movie
